2b78bfae074e57b286e2b756eda17ac36a622630
gabrielysilva27-commits <gabrielysilva27@icloud.com>
2026-09-04 15:24:51 -0300

    Conclui importação de ações pendentes

---
+++ assets/js/imported-action-history-pending.js
@@ -0,0 +1,25 @@
+import { IMPORTED_ACTION_HISTORY_PENDING_01 } from "./imported-action-history-pending-01.js?v=20260904-01";
+import { IMPORTED_ACTION_HISTORY_PENDING_02 } from "./imported-action-history-pending-02.js?v=20260904-01";
+import { IMPORTED_ACTION_HISTORY_PENDING_03 } from "./imported-action-history-pending-03.js?v=20260904-01";
+import { IMPORTED_ACTION_HISTORY_PENDING_04 } from "./imported-action-history-pending-04.js?v=20260904-01";
+import { IMPORTED_ACTION_HISTORY_PENDING_05 } from "./imported-action-history-pending-05.js?v=20260904-01";
+import { IMPORTED_ACTION_HISTORY_PENDING_06 } from "./imported-action-history-pending-06.js?v=20260904-01";
+import { IMPORTED_ACTION_HISTORY_PENDING_07 } from "./imported-action-history-pending-07.js?v=20260904-01";
+import { IMPORTED_ACTION_HISTORY_PENDING_08 } from "./imported-action-history-pending-08.js?v=20260904-01";
+import { IMPORTED_ACTION_HISTORY_PENDING_09 } from "./imported-action-history-pending-09.js?v=20260904-01";
+import { IMPORTED_ACTION_HISTORY_PENDING_10 } from "./imported-action-history-pending-10.js?v=20260904-01";
+import { IMPORTED_ACTION_HISTORY_PENDING_11 } from "./imported-action-history-pending-11.js?v=20260904-01";
+
+export const IMPORTED_ACTION_HISTORY_PENDING = [
+  ...IMPORTED_ACTION_HISTORY_PENDING_01,
+  ...IMPORTED_ACTION_HISTORY_PENDING_02,
+  ...IMPORTED_ACTION_HISTORY_PENDING_03,
+  ...IMPORTED_ACTION_HISTORY_PENDING_04,
+  ...IMPORTED_ACTION_HISTORY_PENDING_05,
+  ...IMPORTED_ACTION_HISTORY_PENDING_06,
+  ...IMPORTED_ACTION_HISTORY_PENDING_07,
+  ...IMPORTED_ACTION_HISTORY_PENDING_08,
+  ...IMPORTED_ACTION_HISTORY_PENDING_09,
+  ...IMPORTED_ACTION_HISTORY_PENDING_10,
+  ...IMPORTED_ACTION_HISTORY_PENDING_11
+];
